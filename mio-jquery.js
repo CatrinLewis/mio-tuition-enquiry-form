e32afd6ac35c98c32ef283b91fe5a50f20cc37c8
CatrinLewis <catrin_elenid@hotmail.com>
2021-04-09 14:50:41 +0100

    first version

---
+++ mio-jquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  $("#next-button").click(function(){
+    $(".second-page").show();
+  });
+
+  $("#next-button").click(function(){
+    $(".hide-post-next").hide();
+  });
+
+  $("#submit-button").click(function(){
+    $(".hidden").show();
+  });
+
+  $("#submit-button").click(function(){
+    $(".hide-post-submit").hide();
+  });
+
+//   $("#instrumentChoice").on('change', function() {
+// 	var selectedValue = $(this).val()
+// 	console.log($(this))
+// 	if(selectedValue == ) {
+//
+// 	} else if () {
+//
+// 	} else {
+//
+// 	}
+// })
+
+});
